4f16ef3b41184cc81c1ff6184f5c8e6256161a37
Андрей Кузнецов <89307449447an@gmail.com>
2022-10-04 02:25:50 +0300

    Добавьте файлы проекта.

---
+++ RegularExpressions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RegularExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time time = new Time();
+            time.time();
+        }
+    }
+}
---
+++ RegularExpressions/Time.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegularExpressions
+{
+    class Time
+    {
+        public void time()
+        {
+            string a = "что то 09:00, потом 05:55 далее 06:65 а так же 19:25";
+            Regex regex = new Regex(@"\b(?:[01][0-9]|2[0-3]):[0-5][0-9]\b");
+            MatchCollection matches = regex.Matches(a);
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                    Console.WriteLine(match.Value);
+            }
+        }
+    }
+}
